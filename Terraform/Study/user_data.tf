a184e9539c54e93a2d2019e3102cf28cc4bd4a7b
junyeongjang <junyeong.dev@gmail.com>
2022-02-25 00:27:16 +0900

    terraform start

---
+++ Terraform/Study/user_data.tf
@@ -0,0 +1,16 @@
+resource "aws_instance" "example" {
+	ami = "ami-40d28157"
+	instance_type = "t2.micro"
+
+
+	user_date = <<-EOF
+		#!/bin/bash
+		echo "Hello, World" > index.html
+		nohup busybox httpd -f -p 8080 &
+		EOF
+
+		
+ 	tags {
+		NAME = "terraform-example"
+ 	}
+}
